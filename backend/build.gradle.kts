9aafb93d886632fb7dbaa1b6600ecbb521c0689f
Silje Hanø Tangen <siljehtangen@outlook.com>
2026-04-18 15:29:06 +0200

    test: add testImpl

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -19,6 +19,7 @@ dependencies {
     runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.mockito.kotlin:mockito-kotlin:5.2.1")
+    testImplementation(kotlin("test"))
 }
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions { freeCompilerArgs += "-Xjsr305=strict"; jvmTarget = "21" }
